849212d3174ad02929965f43ba18df12a8b53ee7
heruni <herunisp@gmail.com>
2026-08-10 14:36:06 +0530

    initialise pages folder, home page n hero section.

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import './App.css'
 import { Button } from './components/ui/button'
 
 function App() {
-  
 
   return (
-    <>
-     <h1>Welcome to React</h1> 
-     <Button onClick={() => alert('Button clicked!')}>Click me</Button>
-    </>
+    <div className="flex flex-col items-center gap-4 p-6">
+     <h1 className="text-center text-2xl font-bold">NestBoard Frontend</h1>
+     <p className="text-center text-muted-foreground">
+       Welcome to the NestBoard frontend!
+     </p>
+     <Button>Click me</Button>
+    </div>
   )
 }
 
---
+++ src/pages/home/components/HeroSection.tsx
@@ -0,0 +1,10 @@
+export function HeroSection() {
+  return (
+    <div className="flex flex-col items-center gap-4 p-6">
+      <h1 className="text-center text-2xl font-bold">NestBoard Frontend</h1>
+      <p className="text-center text-muted-foreground">
+        Welcome to the NestBoard frontend!
+      </p>
+    </div>
+  )
+}
