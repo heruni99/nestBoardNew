10464e745c87a62f96cf98cdf30dec22fbe2e2d2
heruni <herunisp@gmail.com>
2026-08-10 14:41:41 +0530

    import herosection to the home page.

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,10 +1,9 @@
+import {HeroSection} from './components/HeroSection'
+
 export function Home() {
   return (
-    <div className="flex flex-col items-center gap-4 p-6">
-      <h1 className="text-center text-2xl font-bold">NestBoard Frontend</h1>
-      <p className="text-center text-muted-foreground">
-        Welcome to the NestBoard frontend!
-      </p>
-    </div>
+    <>
+      <HeroSection />
+    </>
   )
 }   
