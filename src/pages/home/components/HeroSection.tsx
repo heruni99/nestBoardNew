858f8cd6a423df4686195c14d11acd9226ab5a46
heruni <herunisp@gmail.com>
2026-08-21 19:05:03 +0530

    add styles hero section using tailwind css.

--- src/pages/home/components/HeroSection.tsx
+++ src/pages/home/components/HeroSection.tsx
@@ -1,36 +1,38 @@
 export function HeroSection() {
   return (
-    <section >
-        <div>
+    <section className = "flex flex-col items-center justify-center px-5 pb-16 text-center bg-black">
+        <div className= "flex flex-col items-center">
             {/*country badge*/}
-            <div> 
+            <div className = "mt-20 mb-5 inline-flex items-center gap-1.5 px-3 py-1 text-sm text-white/90"> 
               🇱🇰 SRI LANKA
             </div>
 
-            <h1>Find your perfect <br/> Co-living Space</h1>
+            <h1 className = "mb-3 max-w-2xl text-7xl leading-tight font-bold text-white">
+              Find your perfect 
+              <br/> 
+              Co-living Space
+            </h1>
 
-            <p>Discover afforrdable, community-driven living spaces Colombo and beyond. Live with people who share you ambitions.</p>
+            <p className = "mb-7 max-w-2xl text-xl leading-relaxed text-white/70">
+              Discover afforrdable, community-driven living spaces Colombo and beyond. Live with people who share you ambitions.
+            </p>
 
             {/*stats*/}
-            <div>
+            <div className ="flex items-center gap-15 text-white">
               <div>
-                <p>6+</p>
-                <p>properties</p>
-              </div>
-
-              <div>
-                <p>100+</p>
-                <p>cities</p>
-              </div>
-
-              <div>
-                <p>42+</p>
-                <p>Happy residents</p>
-              </div>
-            </div>
-
-
+              <p className="text-5xl leading-none font-bold">6+</p>
+            <p className="mt-1 text-sm text-orange-200">Properties</p>
+          </div>
+          <div>
+            <p className="text-5xl leading-none font-bold">3</p>
+            <p className="mt-1 text-sm text-orange-200">Cities</p>
+          </div>
+          <div>
+            <p className="text-5xl leading-none font-bold">42+</p>
+            <p className="mt-1 text-sm text-orange-200">Happy Residents</p>
+          </div>
         </div>
+      </div>
     </section>
   )
 }
