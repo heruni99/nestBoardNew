d7d0cd1eddbfee4eb0f01988b125f4d22016126a
heruni <herunisp@gmail.com>
2026-08-28 03:48:01 +0530

    add home.tsx to search filter and property card list

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,21 +1,15 @@
-import { PropertyCard } from '@/components/common/propertyCard'
-import {HeroSection} from './components/HeroSection'
+import { HeroSection } from "./components/HeroSection"
+import { SearchFilters } from "./components/SearchFilters"
+import { PropertyList } from "./components/PropertyList"
+import { properties } from "@/data/properties"
+
 
 export function Home() {
   return (
     <>
       <HeroSection />
-      
-      <div className="grid grid-cols-3 gap-3">
-        <PropertyCard
-          image="https://images.unsplash.com/photo-1545324418-cc1a3fa10c00?w=400&h=500&fit=crop"
-          title="Green Villa Colombo"
-          location="Colombo, Sri Lanka"
-          type="House"
-          price="LKR 25K"
-          rating="4.5"
-        />
-      </div>
+      <SearchFilters />
+      <PropertyList properties={properties} />
     </>
   )
-}   
+}
