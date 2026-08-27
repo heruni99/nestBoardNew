1e9220bc0b856901d46545d97b6dc08a772fdf1a
heruni <herunisp@gmail.com>
2026-08-27 23:29:30 +0530

    add baddge image and details to the property card component,

--- src/components/common/propertyCard.tsx
+++ src/components/common/propertyCard.tsx
@@ -1,6 +1,32 @@
+import { Star } from "lucide-react"
+import { Badge } from "../ui/badge"
+import { Card } from "../ui/card"
+
 export function PropertyCard() {
-    return (
-        <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        </div>
-    )
+  return (
+    <Card>
+      {/* Background image */}
+      <img
+        src={
+          "https://images.unsplash.com/photo-1545324418-cc1a3fa10c00?w=400&h=500&fit=crop"
+        }
+        alt={"title"}
+      />
+      {/* Rating badge */}
+      <Badge>
+        <Star />
+      </Badge>
+
+      {/* Bottom info */}
+      <div>
+        <Badge>House</Badge>
+        <h3>Green Villa Colombo</h3>
+        <p>Colombo, Sri Lanka</p>
+        <p>
+          <span>LKR 25K</span>
+          <span> /Month</span>
+        </p>
+      </div>
+    </Card>
+  )
 }
