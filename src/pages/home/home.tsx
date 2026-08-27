782d6ed543efc228e29fc2ccf2877e048d0c6137
heruni <herunisp@gmail.com>
2026-08-28 02:43:52 +0530

    add props

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -5,7 +5,17 @@ export function Home() {
   return (
     <>
       <HeroSection />
-      <PropertyCard />
+      
+      <div className="grid grid-cols-3 gap-3">
+        <PropertyCard
+          image="https://images.unsplash.com/photo-1545324418-cc1a3fa10c00?w=400&h=500&fit=crop"
+          title="Green Villa Colombo"
+          location="Colombo, Sri Lanka"
+          type="House"
+          price="LKR 25K"
+          rating="4.5"
+        />
+      </div>
     </>
   )
 }   
