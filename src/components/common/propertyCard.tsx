f632c1ef9e6ac108976eead75cc7226494ab0879
heruni <herunisp@gmail.com>
2026-08-27 20:20:29 +0530

    add property card to the home tsx.

---
+++ src/components/common/propertyCard.tsx
@@ -0,0 +1,6 @@
+export function PropertyCard() {
+    return (
+        <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        </div>
+    )
+}
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,10 +1,11 @@
+import { PropertyCard } from '@/components/common/propertyCard'
 import {HeroSection} from './components/HeroSection'
 
 export function Home() {
   return (
     <>
       <HeroSection />
-      
+      <PropertyCard />
     </>
   )
 }   
