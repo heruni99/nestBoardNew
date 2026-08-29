b77ba4347e3521853fddce4e4e7267cb9de3b796
heruni <herunisp@gmail.com>
2026-08-29 20:51:24 +0530

    remove app.css and add styles inter index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+
 import { Home } from './pages/home/home'
 
 function App() {
--- src/pages/home/components/HeroSection.tsx
+++ src/pages/home/components/HeroSection.tsx
@@ -1,35 +1,37 @@
 import heroImage from '@/assets/hero.png'
 export function HeroSection() {
   return (
-    <section className = " relative flex w-full min-h-125 flex-col items-center justify-center px-5 pb-16 text-center bg-black" 
-    style={{
-      backgroundImage: `url(${heroImage})`, 
-      backgroundSize: 'cover', 
-      backgroundPosition: 'center'
+    <section
+      className="relative flex flex-col items-center justify-center text-center px-5 pt-10 pb-16"
+      style={{
+        backgroundImage: `url(${heroImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
       }}
     >
-      {/*dark overlay*/}
-        <div className="z-10 absolute inset-0 bg-black/55"></div>
-        <div className= "z-20 relative flex flex-col items-center">
-            {/*country badge*/}
-            <div className = "mt-20 mb-5 inline-flex items-center gap-1.5 px-3 py-1 text-sm text-white/90"> 
-              🇱🇰 SRI LANKA
-            </div>
+      {/* Dark overlay */}
+      <div className="z-10 absolute inset-0 bg-black/55" />
+      <div className="z-20 flex flex-col items-center">
+        {/* Country badge */}
+        <div className="mt-20 mb-5 inline-flex items-center gap-1.5 px-3 py-1 text-sm text-white/90">
+          🇱🇰 SRI LANKA
+        </div>
 
-            <h1 className = "mb-3 max-w-2xl text-7xl leading-tight font-bold text-white">
-              Find your perfect 
-              <br/> 
-              Co-living Space
-            </h1>
+        <h1 className="mb-3 max-w-2xl text-7xl leading-tight font-bold text-white">
+          Find Your Perfect
+          <br />
+          Co-Living Space
+        </h1>
 
-            <p className = "mb-7 max-w-2xl text-xl leading-relaxed text-white/70">
-              Discover afforrdable, community-driven living spaces Colombo and beyond. Live with people who share you ambitions.
-            </p>
+        <p className="mb-7 max-w-2xl text-xl leading-relaxed text-white/70">
+          Discover affordable, community-driven co-living spaces across Colombo
+          and beyond. Live with people who share your ambitions.
+        </p>
 
-            {/*stats*/}
-            <div className ="flex items-center gap-15 text-white">
-              <div>
-              <p className="text-5xl leading-none font-bold">6+</p>
+        {/* Stats */}
+        <div className="flex items-center gap-15 text-white">
+          <div>
+            <p className="text-5xl leading-none font-bold">6+</p>
             <p className="mt-1 text-sm text-orange-200">Properties</p>
           </div>
           <div>
