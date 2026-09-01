6a0710d0bd6ab0550870b76f1f3015d5ffc0a431
heruni <herunisp@gmail.com>
2026-09-01 23:42:04 +0530

    add features to search filter.

--- src/pages/home/components/SearchFilters.tsx
+++ src/pages/home/components/SearchFilters.tsx
@@ -3,10 +3,11 @@ import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 
 type SearchFiltersProps = {
+  searchQuery: string
   onSearchChange: (value: string) => void
 }
 
-export function SearchFilters({onSearchChange}: SearchFiltersProps) {
+export function SearchFilters({ searchQuery, onSearchChange }: SearchFiltersProps) {
   return (
     <div className="relative z-20 -mt-7 px-4 w-full">
       <div className="rounded-2xl bg-white p-8 shadow-xl">
@@ -15,6 +16,7 @@ export function SearchFilters({onSearchChange}: SearchFiltersProps) {
           <div className="relative flex-1">
             <Search className="pointer-events-none absolute top-1/2 left-3 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
             <Input
+              value={searchQuery}
               placeholder="Search by property name or city..."
               className="h-10 rounded-xl border-gray-200 pl-9"
               onChange={(e) => onSearchChange(e.target.value)}
